5467f609652f6ef959142b19ec7307d4d2a0e878
vortix2950 <vortix2950@ksu.edu>
2024-09-12 11:18:05 +0300

    demo

---
+++ script.js
@@ -0,0 +1,36 @@
+let sentence = document.getElementById("text")
+
+function changeTextContent(){
+
+    sentence.textContent="This is changed content"
+}
+function increaseFontSize(){
+    let old_size = parseFloat(window.getComputedStyle(sentence, null).getPropertyValue('font-size'));
+    sentence.style.fontSize= (old_size+5).toString() + "px"
+
+}
+function decreaseFontSize(){
+    let old_size = parseFloat(window.getComputedStyle(sentence, null).getPropertyValue('font-size'));
+    sentence.style.fontSize= (old_size-5).toString() + "px"
+
+}
+function randomColor(){
+    let r = Math.floor((Math.random()*255))
+    let g = Math.floor((Math.random()*255))
+    let b = Math.floor((Math.random()*255))
+
+    let color = `rgb(${r},${g},${b})`
+    sentence.style.color=color
+
+
+}
+function randomBgColor(){
+    let r = Math.floor((Math.random()*255))
+    let g = Math.floor((Math.random()*255))
+    let b = Math.floor((Math.random()*255))
+
+    let color = `rgb(${r},${g},${b})`
+    sentence.style.backgroundColor=color
+
+
+}
